7c8569e5cf0ccac21f2a3ec2e8791d15dfc26740
prashantmane17 <prashantmn47@gmail.com>
2025-03-28 18:38:16 +0530

    products are added and implemented filter function

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const productContainer = document.getElementById("all_products_cards");
+let products = [];
+const intialCall = async () => {
+    const resonse = await fetch("data/products.json");
+    const data = await resonse.json();
+    products = data;
+    return data;
+}
+intialCall().then(data => {
+    data.forEach(product => {
+        product_cards(product)
+
+    });
+}).catch(error => {
+    console.error("err")
+})
+
+function product_cards(product) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product_card");
+    productCard.innerHTML = ` <div class="product-image">
+                    <img src=${product.image_src} class=""
+                        alt=${product.name} />
+                </div>
+                <div class="product-details">
+                    <p class="product-title">${product.vendor}</p>
+                    <p class="product-name">${product.name}</p>
+                    <div class="product-pricing">
+                        <p class="price">$${product.price}</p>
+                        <p class="mrp-price"> $${product.compare_at_price}</p>
+                        <p class="discount">(50% OFF)</p>
+                    </div>
+                </div>`
+    productContainer.appendChild(productCard);
+}
+const filter_options = document.querySelectorAll(".filter_option");
+
+filter_options.forEach(option => {
+    option.addEventListener('click', (e) => {
+        productContainer.innerHTML = "";
+        productContainer.classList.remove("empty_products")
+        const category = e.target.dataset.category;
+        console.log(category, "ciiiii", products)
+        products.forEach(product => {
+            if (product.tag === category) {
+                product_cards(product)
+            }
+        })
+        const filteredProduct = document.querySelectorAll(".product_card");
+        if (filteredProduct.length === 0) {
+            productContainer.classList.add("empty_products")
+            productContainer.innerHTML = "<div class='empty_msg'>Products not found</div>";
+        }
+    })
+})

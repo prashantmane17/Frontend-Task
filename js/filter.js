4019973fa2d45848a1db4c167f9a35a86d9290b4
Prashant Mane <166215795+prashantmane17@users.noreply.github.com>
2025-04-04 12:20:49 +0530

    filter and resposive
    * implemented cart Overlay
    
    * changing filter name and code reduced
    
    * responsive design
    
    * add to cart message implementd
    
    * filter and resposive
    
    * adding product to cart and changing the quantity
    
    * cartfooter and the dyanmic cart items count

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,10 @@
 const productContainer = document.getElementById("all_products_cards");
 const filter_options = document.querySelectorAll(".filter_option");
+const filterLabels = document.querySelector(".filterLabels");
 const totalProduct = document.getElementById("total_products");
+
+const filters_label = document.getElementById("filters_label");
+
 const filterName = document.getElementById("change_filter_name");
 let products = [];
 const intialCall = async () => {
@@ -9,8 +13,37 @@ const intialCall = async () => {
     const sortedData = data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
     products = sortedData;
     sortedProducts("all", sortedData)
+
 }
 intialCall();
+function handleToggle(event) {
+    event.stopPropagation();
+    filterLabels.classList.toggle('visible');
+    filterLabels.classList.toggle('hidden');
+}
+
+function handleOutsideClick(event) {
+    if (!filterLabels.contains(event.target) && !filters_label.contains(event.target)) {
+        filterLabels.classList.add('hidden');
+        filterLabels.classList.remove('visible');
+    }
+}
+
+function MobileFilter() {
+    if (window.innerWidth < 768) {
+        filters_label.addEventListener('click', handleToggle);
+        document.addEventListener('click', handleOutsideClick);
+    } else {
+        filters_label.removeEventListener('click', handleToggle);
+        document.removeEventListener('click', handleOutsideClick);
+        filterLabels.classList.remove('hidden');
+        filterLabels.classList.remove('visible');
+    }
+}
+
+MobileFilter();
+window.addEventListener('resize', MobileFilter);
+
 document.getElementById("sort_select").addEventListener("change", (e) => {
     const value = e.target.value;
     if (value === "low_high") {
